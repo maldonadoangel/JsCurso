537ae3cfe4c5b64c0711eeaea7b99c15e704fdd3
Angel Morales <maldonadoangel7069@gmail.com>
2022-07-22 04:28:34 -0600

    Uso de la consola

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
 
+let a = 20,
+    b = 30,
+    c = 40,
+    x = 'Hola',
+    z = 'Angel';
 
-//console.log('Hola Mundo');
-
-//variables
-let a = 10;
-//Ya no se utiliza var no es buena practica
-//el scope de var es a nivel muy amplio
-var b = 10;
-const c = 10;
-
-let total = a + b;
-
-console.log(a);
-console.log(b);
-//Como ver que tipo de variable es, utilizamos typeof
-console.log(typeof(c));
-console.log('El total es de la suma del valor a + b: ' + total);
+const saludo = x + ' ' + z;
+    //el metodo table, nos permite mostrar como un arreglo los valores que le pasemos
+console.table({a,b,c,x,z});
+console.log(saludo);
