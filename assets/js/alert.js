5686baa776ab7484355e5388398c253c69ad5b9d
Angel Morales <maldonadoangel7069@gmail.com>
2022-07-23 04:48:30 -0600

    Alert, prompt, confirm

---
+++ assets/js/alert.js
@@ -0,0 +1,18 @@
+
+
+/*
+alert('Hola Mundo');
+
+let nombre = prompt('¿Cual es tu nombre?', 'Escribe tu nombre');
+
+alert('Tu nombre es: ' + nombre);
+
+*/
+
+
+//Nos permite mostrar una ventana con dos opciones, cancelar y aceptar
+//si seleccionamos cancelar nos retornara un false y si aceptamos nos devolvera true
+//en pocas palabras un booleano
+let opcion = confirm('Esta seguro de borrar esto?');
+
+alert(opcion);
