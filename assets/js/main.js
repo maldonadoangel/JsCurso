2830ef0006efd52c96fb7046a046c6a2db93c43b
Angel Morales <maldonadoangel7069@gmail.com>
2022-07-22 23:09:22 -0600

    Ordenamiento de archivos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 
+alert('Hola Mundo');
+
+
 let a = 20,
     b = 30,
     c = 40,
@@ -8,4 +11,9 @@ let a = 20,
 const saludo = x + ' ' + z;
     //el metodo table, nos permite mostrar como un arreglo los valores que le pasemos
 console.table({a,b,c,x,z});
-console.log(saludo);
+console.log(saludo); 
+
+var miNombre = 'Angel Morales';
+
+x = 'Cambio de valor';
+console.log(x);
