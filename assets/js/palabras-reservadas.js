f5cf7595b092986249c99b93ee1048b41d8902c9
Angel Morales <maldonadoangel7069@gmail.com>
2022-07-30 07:51:12 -0600

    Palabras reservadas en Js

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,19 @@
+
+
+//Las palabras reservadas son palabras que tienen un uso especifico
+
+let constante = 123;
+
+//const
+//if
+//true
+//while
+//for()
+
+
+//no se puede crear variables que empiezen con numeros
+/* 
+
+
+
+*/
